6f7e74933eff5f78060fa4676b1d4bdbd8d5211d
sbouden16 <sergiocortessat@gmail.com>
2021-12-01 16:20:25 +0100

    Add login animation

--- src/CSS,HTML/FormWaveAnimation/FormWaveAnimation.tsx
+++ src/CSS,HTML/FormWaveAnimation/FormWaveAnimation.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import './style.css'
+import React from "react";
+import "./style.css";
 
 const FormWaveAnimation = () => {
-    return (
-        <div>
-            <h1>FormWaveAnimation</h1>
-        </div>
+    const handleSubmit = (e:any) => {
+    
+    e.preventDefault();
+    console.log(e.target);
+    }
+
+  const labelSpan = (word:string) => (
+      word.split('').map((letter, index) => {
+          return <span key={index} style={{transitionDelay: `${index * 50}ms`}}>{letter}</span>
+      }
     )
-}
+  )
+
+  console.log(labelSpan("Hello"));
+  return (
+    <div className="container">
+      <h1>Please Login</h1>
+      <form onSubmit={(e:any) => handleSubmit(e)}>
+        <div className="form-control">
+          <input type="text" required />
+          {/* <label>Email</label> */}
+          <label>
+            {/* <span style={{transitionDelay: '0ms'}}>E</span>
+              <span style={{transitionDelay: '50ms'}}>m</span>
+              <span style={{transitionDelay: '100ms'}}>a</span>
+              <span style={{transitionDelay: '150ms'}}>i</span>
+              <span style={{transitionDelay: '200ms'}}>l</span> */}
+
+              {labelSpan('Email')}
+        </label>
+        </div>
+
+        <div className="form-control">
+          <input type="password" required />
+          {/* <label>Password</label> */}
+          <label>
+              {labelSpan('Password')}
+        </label>
+        </div>
+
+        <button type="submit" className="btn">Login</button>
+        <p className="text">
+          Dont have an account?
+          <a href="#">Register</a>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default FormWaveAnimation;
